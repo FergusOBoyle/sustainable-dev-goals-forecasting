a700c3b8b7c91b83d2b0d69dd79614802cb02758
Fergus <fergus.oboyle@gmail.com>
2019-06-18 16:23:46 +0100

    Optimise window_data for speed
    Removed one of the layering of for loops in the windowing function.
    Increase x35 in speed. Refactor also accomaadates integrating the
    imputation method with the window which will be required shortly.

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -22,57 +22,68 @@ def window_data(data, lag=5,num_windows=3, step=1, predict_year=2010, target=Non
         step: the delta between the windows. 1 will mean that there is maximum overlap between windows.
         predict_year: the year that we are targetting
         target: feature to be used as target
-    
+
     Returns:
         training_data_regressors
         training_data_targets
         test_data_regressors
         test_data_targets
     """
-    
+
     assert(target in list(data.columns.values)), "Target should be in the input dataframe"
-    
+
     countries_in_data = list(data.index.levels[0]) 
-    
-    #Create an empty test and training dataframes
+
+    #Create empty test and training dataframes
     regressors_index = pd.MultiIndex.from_product([countries_in_data,
-                                  list(range(1,num_windows+1)), list(range(1,lag+1))],
-                                 names=[u'country', u'window', u'lag'])
-    target_index = pd.MultiIndex(levels=[[],[]],
-                           codes=[[],[]],
-                           names=[u'country', u'window'])
-    test_regressors_index =  pd.MultiIndex.from_product([countries_in_data,list(range(1,lag+1))],
-                           names=[u'country', u'lag'])
+                                                   list(range(1,num_windows+1)), 
+                                                   list(range(1,lag+1))],
+                                                  names=[u'country', u'window', u'lag'])
+
+    target_index = pd.MultiIndex.from_product([countries_in_data,
+                                               list(range(1,num_windows+1))],
+                                              names=[u'country', u'window'])
+
+    test_regressors_index =  pd.MultiIndex.from_product([countries_in_data,
+                                                         list(range(1,lag+1))],
+                                                        names=[u'country', u'lag'])
+
     test_target_index = countries_in_data   
     training_data_regressors = pd.DataFrame(index=regressors_index, columns=data.columns)
     training_data_targets = pd.DataFrame(index=target_index, columns=[target])
     test_data_regressors = pd.DataFrame(index=test_regressors_index, columns=data.columns)
     test_data_targets = pd.DataFrame(index=test_target_index, columns=[target])
-    
-    #fill out the training dataframes : training_data_regressors and training_data_targets
-    #..and the testing dataframes     : test_data_regressors and test_data_targets
-    for country in countries_in_data:
-        test_data_targets.loc[country, target] =  data.loc[(country,str(predict_year)), target]
 
-        test_data_regressors.loc[(country,1):(country,lag+1),:] = \
-            data.loc[(country,str(predict_year-lag)):(country,str(predict_year-1)), :].values       
-            
-        for window in range(1,num_windows+1):
-            year= predict_year - window       
-            #Add the target value for the spilt to the test_data 
-            training_data_targets.loc[(country, window),:] = data.loc[(country,str(year)), target]
-    
-            training_data_regressors.loc[(country,window,1):(country,window,lag+1),:] = \
-                data.loc[(country,str(year-lag)):(country,str(year-1)), :].values
-    
+    #PLACEHOLDER FOR FIRST PASS AT MISSING VALUE IMPUTATION
+
+    #Fill in the target test set (this relates to the target year)
+    idx = pd.IndexSlice
+    test_data_targets.loc[:, target] =  data.loc[idx[:,str(predict_year)], target].values
+
+    #Fill in the training test test
+    test_data_regressors.loc[:,:] = \
+            data.loc[idx[:,str(predict_year-lag):str(predict_year-1)], :].values
+
+    #Now it's time to start moving back in time and filling out our training datasets
+
+    #Each increment of window represents moving back a year in time
+    for window in range(1,num_windows+1):
+        year = predict_year - window
+
+        #PLACEHOLDER FOR FURTHER PASSES AT MISSING VALUE IMPUTATION
+
+        training_data_targets.loc[idx[:,window],:] = data.loc[idx[:,str(year)], target].values
+
+        training_data_regressors.loc[idx[:,window,1:lag+1],:] = \
+                data.loc[idx[:,str(year-lag):str(year-1)], :].values
+
     #According to pandas docs on multiIndex usage: For objects to be indexed and sliced effectively, they need to be sorted.
     training_data_regressors = training_data_regressors.sort_index()
     training_data_targets = training_data_targets.sort_index()
     test_data_targets = test_data_targets.sort_index()
-    
+
     #unstacking the input features. Each row will now represent a set of features.
     training_data_regressors  = training_data_regressors.unstack(level=2)
     test_data_regressors  = test_data_regressors.unstack(level=1)
-    
-    
+        
     return training_data_regressors, training_data_targets, test_data_regressors, test_data_targets
