4fa75f1b71e0392eee54c7870dda61df53785252
Fergus <fergus.oboyle@gmail.com>
2019-06-24 15:03:09 +0100

    Clean up and comment interpolator method

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -2,23 +2,35 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
 
-def impute_data_interpolation(data, upto_year):
+def impute_data_interpolation(data, upto_year, method):
     """
-        
+    Interpolation of indicator time-series for each country seperately in indicator dataFrame 
+
+    Args:
+        data: multiIndex dataFrame of feature data with index of (country, year) and columns of the feature names.
+        upto_year: Year up to and including to interpolate
+        method: method of interpolation to be passed on to the pandas interpolation function
+
+    Returns:
+        interpolated dataFrame
     """
+
+    #Protect data in caller 
     data_local = data.copy()
     
-    for country in data.index.levels[0]:
-        
-        
+    #Interpolation. For loop could be removed if data restructured to have time-series on .. 
+    # rows (i.e Country and indicator in Index and year as column) 
+    for country in data.index.levels[0]:    
         data_local.loc[(country):(country,str(upto_year)),:] = \
-            data_local.loc[(country):(country,str(upto_year)),:].interpolate(method='linear' ,limit_direction='both').values  
+            data_local.loc[(country):(country,str(upto_year)),:]. \
+            interpolate( method=method, limit_direction='both').values  
     
-    
-    idx = pd.IndexSlice
-    
+
+    #Use mean of indicator for any remaining missing values   
+    idx = pd.IndexSlice   
     imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-    data_local.loc[idx[:,:str(upto_year)],:] = imp_mean.fit_transform(data_local.loc[idx[:,:str(upto_year)],:])
+    data_local.loc[idx[:,:str(upto_year)],:] = \
+        imp_mean.fit_transform(data_local.loc[idx[:,:str(upto_year)],:])
     #data_local.loc[idx[:,:str(upto_year)],:] = imputed_subset
     
     #Set remaining missing values to 0. (This should be replaced by mean imputation ASAP)
@@ -31,7 +43,7 @@ def impute_data_interpolation(data, upto_year):
 
 def window_data(data, lag=5,num_windows=3, step=1, predict_year=2010, target=None, impute_type=None):
     """
-    Split up input feature dataframe into windowed data.
+    Split up input feature dataFrame into windowed data.
 
     Bug: Lags are wrong way around. For example if window width of 5 is specified (lag=5) then the lag=1 column..
         gives the 5th value while lag=5 gives the 1st value in the time window.
@@ -81,7 +93,7 @@ def window_data(data, lag=5,num_windows=3, step=1, predict_year=2010, target=Non
         #Redo Imputation every time we move back a year
         #This maintains the requirement not to use information from future years in our imputations 
         if impute_func is not None:
-            data_imp = impute_func(data, upto_year=year-1 )
+            data_imp = impute_func(data, upto_year=year-1, method='linear' )
         else:
             data_imp = data
 
